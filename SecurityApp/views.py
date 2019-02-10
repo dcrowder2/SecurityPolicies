3fd6a4f3cbc00c3bab2e5b2db683093baee64e46
dcrowder2 <dcrowder2@alaska.edu>
2019-02-09 22:11:49 -0900

    Added a login page as the root page, as well as a way to do authentication

--- SecurityApp/views.py
+++ SecurityApp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 
 def home(request):
+
+    if request.method == "POST":
+        if request.POST['logon']:
+            return logon(request)
+
     return render(request, 'SecurityApp/home.html')
 # Create your views here.
+
+
+def logon(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    authentication = authenticate(request, username=username, password=password)
+    if authentication is not None:
+        login(request, authentication)
+        # the only page we can redirct to is the program page, so no need for it to be dynamic
+        return HttpResponseRedirect('programs')
+    context = {'logon_failed': True}
+    return render(request, 'SecurityApp/home.html', context=context)
