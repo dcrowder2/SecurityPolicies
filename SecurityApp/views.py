7afe1bbb2b5b880f0d6f1b153b4551f01cb639b7
dcrowder2 <dcrowder2@alaska.edu>
2019-02-10 09:38:19 -0900

    Added a logon table to keep track of logon attempts

--- SecurityApp/views.py
+++ SecurityApp/views.py
@@ -3,17 +3,19 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 
 
+
 def home(request):
 
     if request.method == "POST":
         if request.POST['logon']:
-            return logon(request)
+            context = { 'logon_attempts': user.attempts}
+            return logon(request, context)
 
     return render(request, 'SecurityApp/home.html')
 # Create your views here.
 
 
-def logon(request):
+def logon(request, context):
     username = request.POST['username']
     password = request.POST['password']
     authentication = authenticate(request, username=username, password=password)
@@ -21,5 +23,6 @@ def logon(request):
         login(request, authentication)
         # the only page we can redirct to is the program page, so no need for it to be dynamic
         return HttpResponseRedirect('programs')
-    context = {'logon_failed': True}
+    context['logon_failed'] = True
+    context['logon_attempts']
     return render(request, 'SecurityApp/home.html', context=context)
