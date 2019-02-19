e2b8b70c6c89689bc55564fa8a20eb5c7750a29e
Dakota Crowder <dakota@deep-tree.com>
2019-02-19 14:50:00 -0900

    Sectioned off programs based on access level Started creating logs for program access

--- programs/views.py
+++ programs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Programs
 from django.contrib.auth.decorators import login_required
+import time, datetime
 # Create your views here.
 
 
@@ -8,11 +9,24 @@ from django.contrib.auth.decorators import login_required
 def programs(request):
 	username = request.user.username
 	group = request.user.groups.all()
+	program_list = Programs.objects.all()
 	context = {
-		'programs': Programs.objects.all(),
-		'user' : username,
-		'group' : group[0]
+		'programs': program_list,
+		'user': username,
+		'group': str(group[0])
 	}
 	if request.method == "POST":
+		file = open("program_access.log", "a")
+		file.write(log_write(username, request.POST['programs']))
+		file.close()
+		context['accessed'] = request.POST['programs']
 		return render(request, 'programs.html', context=context)
 	return render(request, 'programs.html', context=context)
+
+
+def log_write(username, application):
+	return_string = "Application access\n"
+	return_string += "Time: " + str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')) + "\n"
+	return_string += "Username: " + username + "\n"
+	return_string += "Application: " + application + "\n----------------------------\n"
+	return return_string
