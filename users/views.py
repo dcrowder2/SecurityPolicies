dc710ba4b1438a54edad075bbb3f822ccec8579c
lgriego1 <47466859+lgriego1@users.noreply.github.com>
2019-02-20 00:15:30 -0900

    Update views.py
    Added check for at least one upper/lowercase letter and one digit. Also saved newly registered users under your Logon model, so they can now login after registering.

--- users/views.py
+++ users/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from SecurityApp import models
+from django.contrib.auth.models import User
+import re
 
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         try:
-            name = form.data['username'].encode('ascii')
-            if form.is_valid():
+            form.data['username'].encode('ascii')
+            if not (re.match(r'(.*[A-Z].*)', form.data['password1']) and re.match(r'(.*[a-z].*)', form.data['password1'])
+                    and re.match(r'(.*\d.*)', form.data['password1'])):
+                messages.error(request, 'Password must contain an uppercase letter, a lowercase letter, and a digit')
+                return redirect('register')
+            elif form.is_valid():
                 form.save()
                 username = form.cleaned_data.get('username')
+                models.Logon(user=User.objects.get(username=username)).save()
                 messages.success(request, f'Account created for {username}')
                 return redirect('apps-home')
         except UnicodeEncodeError:
