643838ed0d13d84b22d4d3a9aa734f6924d8eba6
lgriego1 <47466859+lgriego1@users.noreply.github.com>
2019-02-20 18:51:07 -0900

    Added testing for our password policies
    I left out the ones that django already catches (short passwords, nonmatching passwords, etc.)

--- users/tests.py
+++ users/tests.py
@@ -33,11 +33,6 @@ class RegisterTestCase(TestCase):
             'password1': 'SecurePassword',
             'password2': 'SecurePassword'
         }
-        non_matching = {
-            'username': 'test',
-            'password1': 'SecurePass167',
-            'password2': 'SecurePass761'
-        }
         non_ascii = {
             'username': 'test',
             'password1': 'SecPass167パスワード',
@@ -54,10 +49,6 @@ class RegisterTestCase(TestCase):
 
         response = self.client.post(reverse('register'), data=no_digit)
         self.assertRedirects(response, '/register/')
-        self.assertEqual(User.objects.count(), 0)
-
-        response = self.client.post(reverse('register'), data=non_matching)
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(User.objects.count(), 0)
 
         response = self.client.post(reverse('register'), data=non_ascii)
